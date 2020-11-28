31a25b90c373f9056bd4d5234257e7c7b8b72ae4
karuna531 <68470642+karuna531@users.noreply.github.com>
2020-11-28 14:08:18 +0545

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,56 @@
+function displayValue(val){
+	document.getElementById("result").value += val;
+
+}
+
+
+function solve(){
+	let x = document.getElementById("result").value;
+	if(x==""){
+		return;
+	}
+	let y =eval(x);
+	document.getElementById("result").value = y;
+}
+
+function clearResult(){
+	document.getElementById("result").value=" ";
+}
+
+const toggleBtn = document.querySelector('.toggle-mode');
+
+
+toggleBtn.addEventListener('click', function() {
+ 
+  document.body.classList.toggle('dark-theme');
+  if (this.innerHTML == "Dark Mode") {
+    this.innerHTML = "Light Mode";
+  } else {
+    this.innerHTML = "Dark Mode";
+  }
+})
+
+function back() {
+    //delete last letter function.
+     result.value = result.value.slice(0, - 1);
+}
+function square(){
+	let a = document.getElementById("result").value;
+	if(a==""){
+		return;
+	}
+	let y =a*a;
+	document.getElementById("result").value = y;
+
+}
+
+
+
+
+
+
+
+
+
+
+
